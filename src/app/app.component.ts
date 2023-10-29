88d2dc618b2cf6fa0ef38b4c5f3c98395a265e5c
Topaz712 <tzam712@gmail.com>
2023-10-29 04:01:10 -0500

    Added: setValue/patchValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,10 +27,24 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
         (status) => console.log(status)
       );
+      this.signupForm.setValue({
+        'userData': {
+          'username': 'Topaz',
+          'email': 'topaz@test.com'
+        },
+        'gender': 'female',
+        'hobbies': []
+      });
+      this.signupForm.patchValue({
+        'userData': {
+          'username': 'Max',
+        }
+      });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
