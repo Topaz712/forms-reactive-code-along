d70991d8af9fcf44811c49b09ccfd3c98de13cce
Topaz712 <tzam712@gmail.com>
2023-10-29 01:20:12 -0500

    Added: arrays of form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,25 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('female')
+      'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
   }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  // get controls() {
+  //   return (this.signupForm.get('hobbies') as FormArray).controls;
+  // } alternatively can set up a getter and use this alternate type casting syntax and then in the html template do *ngFor="let hobbyControl of controls; let i = index"
 }
